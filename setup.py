dce06a2122979aa999d83259b7e34c4c4c3c3aa8
Edlison <bolin.edlison.shen@gmail.com>
2025-08-26 23:00:37 -0400

    update action

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="pygip",
-    version="0.1.0",
+    name="PyGIP",
+    version="1.0.0",
     author="Bolin Shen",
     author_email="blshen@fsu.edu",
     description="A Python package for Graph Intellectual Property Protection",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/labrai/pygip",
+    url="https://github.com/LabRAI/PyGIP",
     packages=find_packages(
-        include=["models*", "datasets*", "utils*"]
+        include=["pygip*"]
     ),
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -26,10 +26,10 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
-        "torch>=1.7.0",
-        "dgl>=0.6.0",
+        "torch>=2.3.0",
+        "dgl>=2.2.1",
         "torch-geometric>=2.0.0",
         "numpy>=1.19.0",
         "scipy>=1.6.0",
